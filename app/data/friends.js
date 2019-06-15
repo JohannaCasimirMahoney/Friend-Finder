4b662740506a01a0a5eed11c2dee9982d9469e73
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-15 16:55:48 -0700

    created the first three data information with scores

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+// Created variable to hold data
+var friends = [
+    {
+        name: "Micheal Phelps",
+        photo: "https://www.tumblr.com/search/micheal+phelps",
+        scores: ["2", "4", "5", "4", "5", "3", "2", "5", "1", "4"]
+    },
+    {
+        name: "Micheal Phelps",
+        photo: "https://www.tumblr.com/search/serena+williams",
+        scores: ["4", "2", "4", "1", "5", "3", "1", "4", "2", "3"]
+    },
+    {
+        name: "Micheal Phelps",
+        photo: "https://www.tumblr.com/search/lebron+james",
+        scores: ["2", "4", "3", "2", "5", "2", "4", "4", "2", "2"]
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]

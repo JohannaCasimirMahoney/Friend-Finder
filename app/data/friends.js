2ec9688abd657c9c06cff3ec9fa166f35879527e
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-15 17:52:18 -0700

    added the other eight names and score

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,54 +2,56 @@
 var friends = [
     {
         name: "Micheal Phelps",
-        photo: "https://www.tumblr.com/search/micheal+phelps",
+        photo: "https://66.media.tumblr.com/tumblr_m7u1maCq311qeao3to1_1280.jpg",
         scores: ["2", "4", "5", "4", "5", "3", "2", "5", "1", "4"]
     },
     {
-        name: "Micheal Phelps",
-        photo: "https://www.tumblr.com/search/serena+williams",
+        name: "Serena Williams",
+        photo: "https://66.media.tumblr.com/c3707c5ef87911a50e97d139ac0ee8a0/tumblr_olfbuwwowN1rjupavo2_640.jpg",
         scores: ["4", "2", "4", "1", "5", "3", "1", "4", "2", "3"]
     },
     {
-        name: "Micheal Phelps",
-        photo: "https://www.tumblr.com/search/lebron+james",
+        name: "Lebron James",
+        photo: "https://66.media.tumblr.com/62c51221cbb19f815e4fd10b93e16254/tumblr_pfwgieQAT51r5w7j2o2_1280.jpg",
         scores: ["2", "4", "3", "2", "5", "2", "4", "4", "2", "2"]
+    },
+    {
+        name: "Christiano Ronaldo",
+        photo: "https://66.media.tumblr.com/ab329aba947c1d4a5eab36a3fac0dc32/tumblr_pi26v9ZFlJ1wckxuyo2_1280.jpg",
+        scores: ["5", "5", "5", "1", "3", "1", "1", "3", "2", "3"]
+    },
+    {
+        name: "Colin Kapernick",
+        photo: "https://66.media.tumblr.com/016408b8350beb3bcbdb2b5565942334/tumblr_nbz88xTwSn1sqmwgvo1_500.jpg",
+        scores: ["1", "5", "3", "5", "4", "3", "5", "5", "2", "2"]
+    },
+    {
+        name: "Mo'ne Davis",
+        photo: "https://66.media.tumblr.com/92189b1bcda094985bdf4d1261debe28/tumblr_nlongfEXy31sijq9co1_1280.jpg",
+        scores: ["5", "5", "3", "1", "4", "5", "5", "3", "2", "3"]
+    },
+    {
+        name: "Kristi Yamaguchi",
+        photo: "https://66.media.tumblr.com/tumblr_ln3xom2eyM1qjqs58o1_1280.jpg",
+        scores: ["2", "5", "4", "5", "5", "2", "3", "2", "4", "1"]
+    },
+    {
+        name: "Usaine Bolt",
+        photo: "https://66.media.tumblr.com/fa00103699d161a154dfccaf60b7b1d2/tumblr_obxhtgm5Xt1uvan7ho2_640.jpg",
+        scores: ["3", "4", "2", "5", "5", "2", "4", "3", "4", "1"]
+    },
+    {
+        name: "Britney Griner",
+        photo: "https://66.media.tumblr.com/e1da6f2b0d62614ada8eae0c6904dbe2/tumblr_mr80fmJFdn1sq0eaao1_1280.jpg",
+        scores: ["3", "4", "2", "5", "5", "1", "2", "3", "4", "1"]
+    },
+    {
+        name: "Chuck Liddell",
+        photo: "https://66.media.tumblr.com/ec70bb3671f0a77be399f147e3619061/tumblr_p6w6o6dslq1stmduxo3_500.jpg",
+        scores: ["3", "3", "1", "3", "4", "5", "2", "5", "1", "2"]
     },
 
 
+];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-]
+module.exports = friends;

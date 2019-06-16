5ca012f2dd5df2409fe238af37a46b4695847e4b
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-16 12:51:04 -0700

    added stactic to make the call to routes

--- server.js
+++ server.js
@@ -7,22 +7,21 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Express app interpret data
+// Body Parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({
-    type: "application/vnd.api+json"
-}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-//api routes
+// Static files allows the call to be made before the routes in order for it to work.
+app.use(express.static('app/public'));
+
+//Routers
 require('./app/routing/apiRoutes.js')(app);
 //html routes
 require('./app/routing/htmlRoutes.js')(app);
 
-// initiates the server 
+// Listening port 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });

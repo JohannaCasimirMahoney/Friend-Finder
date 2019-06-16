3b6ca8dc031e5a7ce6d9b7d12abd80ff5760b341
Ghost <ghost@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-15 18:09:43 -0700

    added get and post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,4 +5,35 @@ module.exports = function (app) {
         res.json(friends);
     });
 
+    app.post('/api/friends', function (req, res) {
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 100
+        };
+
+        // user input sent data from survey.html
+        var newFriend = req.body;
+        var newScores = newFriend.scores;
+
+        // loop through all the friends. 
+        for (var i = 0; i < friends.length; i++) {
+            var scoreDifference = 0;
+
+            // loop through scores of each friend to get score difference
+            for (var h = 0; h < friends[i].scores[h]; h++) {
+                scoreDifference += Math.abs(parseInt(newScores[h]) - parseInt(friends[i].scores[h]));
+
+                // set "best match" at first loop then updates each subsequent loop
+                if (scoreDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = scoreDifference;
+                }
+            }
+        }
+        friends.push(newFriend);
+        res.json(bestMatch);
+    });
 }

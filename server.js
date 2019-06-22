334256e2b61cc5c0d77778d01cb627803c2df7ec
Ghost Ghost <ghost77@Ghosts-MBP.hsd1.ca.comcast.net>
2019-06-21 20:28:33 -0700

    deleted extra app use and body parser

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 // express.
 var express = require("express");
-var bodyParser = require("body-parser");
 var path = require("path");
+var bodyParser = require("body-parser");
+
+//const express = require('express')
+const app = express()
 
 // Express App 
-var app = express();
+// var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Body Parser app to data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-app.use(bodyParser.text());
-app.use(bodyParser.json({
-    type: "application/vnd.api+json"
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+app.use(express.static("app/public"));
+
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({
+//     type: "application/vnd.api+json"
+// }));
 
 
 //Routes
@@ -27,3 +36,5 @@ require('./app/routing/htmlRoutes.js')(app);
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+// app.listen(PORT,() => console.log(`listening on PORT ${PORT}`))
